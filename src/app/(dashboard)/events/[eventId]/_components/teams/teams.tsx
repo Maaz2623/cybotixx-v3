850c0797999864efea9db5d5d60678ae34fc4ab1
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2024-11-19 11:14:02 +0530

    modified:   bun.lockb 	modified:   src/app/(dashboard)/events/[eventId]/_components/participants/columns.tsx 	modified:   src/app/(dashboard)/events/[eventId]/_components/teams/teams.tsx 	modified:   src/app/(dashboard)/events/[eventId]/_components/winners/columns.tsx 	modified:   src/app/(dashboard)/members/_components/member-card.tsx

--- src/app/(dashboard)/events/[eventId]/_components/teams/teams.tsx
+++ src/app/(dashboard)/events/[eventId]/_components/teams/teams.tsx
@@ -11,6 +11,15 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { columns, Member } from "./columns";
+import { DataTable } from "./data-table";
+import { InfoIcon } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 const Teams = ({ eventId }: { eventId: string }) => {
   const { data } = useGetFullSlots({ eventId: eventId as Id<"events"> });
@@ -20,47 +29,68 @@ const Teams = ({ eventId }: { eventId: string }) => {
 
   if (!data) return;
 
-  const fullSlots = data.slots.filter(
-    (slot) => slot.members.length >= event.eventTeamMaxMembers
-  );
 
-  const numbers = Array.from({ length: 100 }, (_, i) => i + 1);
+  const slotMembers = data.slots
+    .filter((slot) => slot.members)
+    .flatMap((slot) => slot.members);
+    
+
+  const formattedSlotMembers: Member[] = slotMembers
+    .filter(
+      (slotMember): slotMember is NonNullable<typeof slotMember> =>
+        slotMember !== null && slotMember !== undefined
+    ) // Type guard to exclude null/undefined
+    .map((slotMember, index) => ({
+      convex_user_id: slotMember._id as Id<"users">,
+      fullName: slotMember.fullName as string,
+      clerkImageUrl: slotMember.clerkImageUrl as string,
+      roleType: slotMember.roleType as Member["roleType"],
+      index,
+    }));
 
   return (
     <div className="min-h-screen border w-full">
       <div className="flex justify-center items-center w-full">
-        <h1 className="text-xl md:text-2xl p-5 font-semibold">
-          {fullSlots.length} Slot(s) Full
+        <h1 className="text-xl md:text-2xl p-5 font-semibold flex items-center gap-2 justify-center">
+          <p>{data.slots.length} Slot(s) full</p>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <InfoIcon className="ml-2 size-4 cursor-pointer hover:text-gray-600 transition-all duration-300" />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>When slots are full, They Appear here</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         </h1>
       </div>
       <div className="border flex flex-wrap justify-center items-start gap-x-4 p-4 gap-y-6 md:p-14">
-        {fullSlots.map((slot) => {
+        {data.slots.map((slot) => {
+          const isFull = slotMembers.length >= event.eventTeamMaxMembers;
+
           return (
             <Dialog key={slot.slotId}>
               <DialogTrigger asChild>
                 <Button className="border-primary border text-white rounded-md bg-primary/50 w-20 h-8 flex justify-center text-xs md:text-sm items-center">
-                  Slot {slot.slotNumber}
+                  {isFull}
                 </Button>
               </DialogTrigger>
               <DialogContent className="w-[90%] rounded-md">
                 <DialogHeader>
-                  <DialogTitle>Join Slot {slot.slotNumber}</DialogTitle>
+                  <DialogTitle className="flex w-full items-center justify-center gap-2">
+                    <>
+                      <p>Slot {slot.slotNumber}</p>
+                      <p className="text-gray-400">
+                        ( {slotMembers.length}/{event.eventTeamMaxMembers} )
+                      </p>
+                    </>
+                  </DialogTitle>
                   <DialogDescription></DialogDescription>
                 </DialogHeader>
-                {/* <div className="w-full space-y-3">
-                  <DataTable data={membersExistInSlot} columns={columns} />
-                  <div className="flex justify-end items-center">
-                    <Button
-                      disabled={loading || isSlotFull}
-                      className="border-primary bg-primary/50 text-white"
-                      onClick={() =>
-                        handleJoin({ slotNumber: slot.slotNumber })
-                      }
-                    >
-                      Join
-                    </Button>
-                  </div>
-                </div> */}
+                <div className="w-full space-y-3">
+                  <DataTable data={formattedSlotMembers} columns={columns} />
+                </div>
               </DialogContent>
             </Dialog>
           );
--- src/app/(dashboard)/members/_components/member-card.tsx
+++ src/app/(dashboard)/members/_components/member-card.tsx
@@ -25,7 +25,6 @@ const MemberCard = ({ convex_user_id }: { convex_user_id: Id<"users"> }) => {
 
   if (!data) return null;
 
-  console.log(data);
 
   return (
     <DialogContent className="rounded-lg h-fit border border-white/20 w-[80%] aspect-square">
