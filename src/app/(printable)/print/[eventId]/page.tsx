23c58003f0233ec4619ffe7d8db4859c0e26ea2e
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2024-12-03 13:45:22 +0530

    modified:   src/app/(printable)/print/[eventId]/page.tsx

--- src/app/(printable)/print/[eventId]/page.tsx
+++ src/app/(printable)/print/[eventId]/page.tsx
@@ -46,6 +46,57 @@ const PrintablePage = () => {
             </tr>
           </thead>
         )}
+        {event.eventName === "Bill Board" && (
+          <thead>
+            <tr className="border">
+              <th className="border p-3 w-[100px] text-center">Sl No</th>
+              <th className="border p-3 w-[500px] text-center">Participants</th>
+              <th className="border p-3 w-[200px] text-center">Relevance</th>
+              <th className="border p-3 w-[200px] text-center">Creativity</th>
+              <th className="border p-3 w-[150px] text-center">Total</th>
+            </tr>
+          </thead>
+        )}
+        {event.eventName == "Pretense" && (
+          <tbody>
+            {slots.map((slot) => {
+              const slotMembers = slot.slotMembers.map((member) => member);
+
+              return (
+                <tr className="border" key={slot.slotId}>
+                  <td>
+                    <div className="flex justify-start w-full h-[100px]">
+                      <div className="border-r flex h-full items-center justify-center">
+                        <p className="-rotate-90">Slot {slot.slotNumber}</p>
+                      </div>
+                      <div className="justify-center items-center flex flex-col flex-1 gap-1">
+                        <div className="h-full flex justify-center gap-x-2 items-center">
+                          <p>{slotMembers[0]?.fullName}</p>
+                          <p className="text-sm">
+                            ({slotMembers[0]?.courseYear}{" "}
+                            {slotMembers[0]?.courseName})
+                          </p>
+                        </div>
+                        <Separator className="separator" />
+                        <div className="h-full flex justify-center gap-x-4 items-center">
+                          <p>{slotMembers[1]?.fullName}</p>
+                          <p className="text-sm">
+                            ({slotMembers[1]?.courseYear}{" "}
+                            {slotMembers[1]?.courseName})
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                  <td className="border"></td>
+                  <td className="border"></td>
+                  <td className="border"></td>
+                  <td className="border"></td>
+                </tr>
+              );
+            })}
+          </tbody>
+        )}
         {event.eventName == "Pretense" && (
           <tbody>
             {slots.map((slot) => {
@@ -87,6 +138,27 @@ const PrintablePage = () => {
           </tbody>
         )}
         {event.eventName == "Break Silence" && (
+          <tbody>
+            {participants.map((participant, index) => {
+              return (
+                <tr className="border" key={participant?._id}>
+                  <td className="">
+                    <p className="w-full text-center">{index + 1}</p>
+                  </td>
+                  <td className="border p-3">
+                    <p className="w-full text-center">
+                      {participant?.fullName}
+                    </p>
+                  </td>
+                  <td className="border"></td>
+                  <td className="border"></td>
+                  <td className="border"></td>
+                </tr>
+              );
+            })}
+          </tbody>
+        )}
+        {event.eventName == "Bill Board" && (
           <tbody>
             {participants.map((participant, index) => {
               return (
